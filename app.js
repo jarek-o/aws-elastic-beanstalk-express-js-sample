b3f0cd7a470f52dd2a2c8143237e470aaaee10e1
jarek osetek <jarek.osetek@gmail.com>
2021-02-20 17:31:09 +0000

    change msg

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('jarek supppper'));
+app.get('/', (req, res) => res.send('Moze sie to jednak uda!'));
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
